0700875482ce454f5ca9cb641a8207f78091f7bd
pelin-a <pelinsualtun@gmail.com>
2023-09-13 23:18:34 +0200

    created database, pulled and displayed data on homepage

--- habersitesi/urls.py
+++ habersitesi/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('contact/', views.contact, name="contact"),
+    path('about/', views.about, name="about"),
+    path('news/', views.news, name="news"),
+    path('add_news/', views.add_news, name="add_news"),
     
 ]
--- habersitesi/views.py
+++ habersitesi/views.py
@@ -1,17 +1,22 @@
 from django.shortcuts import render
+from .models import News
 
 # Create your views here.
 def home(request):
+    all_news = News.objects.all
     
-    news_data=[{
-        'title':"hello",
-        'description':"description"
-        
-    },
-    {'title':"hello",
-    'description':"description"   
-    }
     
-    ]
-    context = {'news_data': news_data}
-    return render(request, 'pages/home.html',context)
+    news_data = {'news_data': all_news}
+    return render(request, 'pages/home.html',news_data)
+
+def contact(request):
+    return render(request, 'pages/contact.html')
+
+def about(request):
+    return render(request, 'pages/about.html')      
+
+def news(request):
+    return render(request, 'pages/news.html')
+
+def add_news(request):
+    return render(request, 'pages/add_news.html')
